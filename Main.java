957df7365cb666e549d63ebac0b33ba0e4987912
youcefB95 <42960822+youcefB95@users.noreply.github.com>
2024-12-01 01:11:29 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, coder95!");
+        System.out.println("Hello, coder95! it is cool");
     }
 }
